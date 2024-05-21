da37cf98298b1af7b77cfecbed51b324eaad455a
meszpetera <peter.meszaros5@stud.uni-corvinus.hu>
2024-05-21 15:37:43 +0200

    Add project files.

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,136 @@
+﻿/*
+' Copyright (c) 2024 Nokedlik
+'  All rights reserved.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+
+using DotNetNuke.Data;
+using DotNetNuke.Entities.Users;
+using DotNetNuke.Framework.JavaScriptLibraries;
+using DotNetNuke.Web.Mvc.Framework.ActionFilters;
+using DotNetNuke.Web.Mvc.Framework.Controllers;
+using Nokedlik.Dnn.suti.Components;
+using Nokedlik.Dnn.suti.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Hotcakes.CommerceDTO.v1;
+using Hotcakes.CommerceDTO.v1.Client;
+using Hotcakes.CommerceDTO.v1.Orders;
+using Hotcakes.CommerceDTO.v1.Contacts;
+
+namespace Nokedlik.Dnn.suti.Controllers
+{
+    [DnnHandleError]
+    public class ItemController : DnnController
+    {
+
+        public ActionResult Delete(int itemId)
+        {
+            ItemManager.Instance.DeleteItem(itemId, ModuleContext.ModuleId);
+            return RedirectToDefaultRoute();
+        }
+
+        public ActionResult Edit(int itemId = -1)
+        {
+            DotNetNuke.Framework.JavaScriptLibraries.JavaScript.RequestRegistration(CommonJs.DnnPlugins);
+
+            var userlist = UserController.GetUsers(PortalSettings.PortalId);
+            var users = from user in userlist.Cast<UserInfo>().ToList()
+                        select new SelectListItem { Text = user.DisplayName, Value = user.UserID.ToString() };
+
+            ViewBag.Users = users;
+
+            var item = (itemId == -1)
+                 ? new Item { ModuleId = ModuleContext.ModuleId }
+                 : ItemManager.Instance.GetItem(itemId, ModuleContext.ModuleId);
+
+            return View(item);
+        }
+
+        [HttpPost]
+        [DotNetNuke.Web.Mvc.Framework.ActionFilters.ValidateAntiForgeryToken]
+        public ActionResult Edit(Item item)
+        {
+            if (item.ItemId == -1)
+            {
+                item.CreatedByUserId = User.UserID;
+                item.CreatedOnDate = DateTime.UtcNow;
+                item.LastModifiedByUserId = User.UserID;
+                item.LastModifiedOnDate = DateTime.UtcNow;
+
+                ItemManager.Instance.CreateItem(item);
+            }
+            else
+            {
+                var existingItem = ItemManager.Instance.GetItem(item.ItemId, item.ModuleId);
+                existingItem.LastModifiedByUserId = User.UserID;
+                existingItem.LastModifiedOnDate = DateTime.UtcNow;
+                existingItem.ItemName = item.ItemName;
+                existingItem.ItemDescription = item.ItemDescription;
+                existingItem.AssignedUserId = item.AssignedUserId;
+
+                ItemManager.Instance.UpdateItem(existingItem);
+            }
+
+            return RedirectToDefaultRoute();
+        }
+
+        [ModuleAction(ControlKey = "Edit", TitleKey = "AddItem")]
+        public ActionResult Index()
+        {
+            var currentUser = UserController.Instance.GetCurrentUserInfo();
+            int a = currentUser.UserID;
+            string bvin = "";
+            IDataContext ctx = DataContext.Instance();
+            var rep = ctx.GetRepository<Order>();
+            string url = "http://20.234.113.211:8107";
+            url = "http://www.dnndev.me/";
+            string key = "1-79771cd1-cb22-4710-a786-b360d8a92c2f";
+            key = "1-f7b5b986-6a80-44a9-9141-003b8559ca85";
+
+            if (Request.Cookies["hotcakes-cartid-1"] != null)
+            {
+                // Delete the existing cookie by setting its expiration date to a past date
+                Response.Cookies["hotcakes-cartid-1"].Expires = DateTime.Now.AddDays(-1);
+            }
+
+            Api proxy = new Api(url, key);
+
+            // call the API to find all orders in the store
+            ApiResponse<List<OrderSnapshotDTO>> response = proxy.OrdersFindAll();
+
+            List<OrderSnapshotDTO> orderSnapshotList = response.Content;
+
+            foreach (var orderSnapshot in orderSnapshotList)
+            {
+                if (orderSnapshot.UserID == a.ToString())
+                {
+                    bvin = orderSnapshot.bvin; break;
+                }
+            }
+
+            if (bvin != "")
+            {
+                HttpCookie cookie = new HttpCookie("hotcakes-cartid-1", bvin);
+
+                // Set the cookie expiration (optional)
+                cookie.Expires = DateTime.Now.AddDays(1); // Expires in one day
+
+                // Add the cookie to the response
+                Response.Cookies.Add(cookie);
+            }
+            
+
+            return View();
+        }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,56 @@
+﻿using DotNetNuke.Common.Utilities;
+using DotNetNuke.ComponentModel.DataAnnotations;
+using DotNetNuke.Entities.Content;
+using System;
+using System.Web.Caching;
+
+namespace Nokedlik.Dnn.suti.Models
+{
+    [TableName("hcc_Order")]
+    [PrimaryKey("Id", AutoIncrement = true)]
+    [Cacheable("Orders", CacheItemPriority.Default, 20)]
+    public class Order
+    {
+        public int Id { get; set; }
+        public Guid Bvin { get; set; }
+        public Guid? AffiliateId { get; set; }
+        public string BillingAddress { get; set; }
+        public string CustomProperties { get; set; }
+        public decimal FraudScore { get; set; }
+        public decimal GrandTotal { get; set; }
+        public decimal HandlingTotal { get; set; }
+        public string Instructions { get; set; }
+        public bool IsPlaced { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public string OrderDiscounts { get; set; }
+        public string OrderDiscountDetails { get; set; }
+        public string OrderNumber { get; set; }
+        public string PaymentStatus { get; set; }
+        public string ShippingAddress { get; set; }
+        public int? ShippingMethodId { get; set; }
+        public string ShippingMethodDisplayName { get; set; }
+        public int? ShippingProviderId { get; set; }
+        public string ShippingProviderServiceCode { get; set; }
+        public string ShippingStatus { get; set; }
+        public decimal ShippingTotal { get; set; }
+        public string ShippingDiscounts { get; set; }
+        public string ShippingDiscountDetails { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal TaxTotal { get; set; }
+        public DateTime TimeOfOrder { get; set; }
+        public string UserEmail { get; set; }
+        public int UserId { get; set; }
+        public int StatusCode { get; set; }
+        public string StatusName { get; set; }
+        public string ThirdPartyOrderId { get; set; }
+        public int StoreId { get; set; }
+        public decimal ItemsTax { get; set; }
+        public decimal ShippingTax { get; set; }
+        public decimal ShippingTaxRate { get; set; }
+        public decimal AdjustedShippingTotal { get; set; }
+        public string UserDeviceType { get; set; }
+        public bool IsAbandonedEmailSent { get; set; }
+        public bool IsRecurring { get; set; }
+        public string UsedCulture { get; set; }
+    }
+}
